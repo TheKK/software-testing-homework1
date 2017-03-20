f851d94f014e2ff1902eb83ee6c6b7a43140204e
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2017-03-20 23:42:30 +0800

    Implement commission problem feature code
    Signed-off-by: Ying-Ruei Liang(KK) <thumbd03803@gmail.com>

--- src/commissionMchine.cpp
+++ src/commissionMchine.cpp
@@ -12,5 +12,21 @@ operator<<(std::ostream& os, const PartsSelled& selled) {
 
 boost::optional<double>
 caculateCommision(PartsSelled selled) noexcept {
-    return boost::none;
+    if ((selled.lockNum < 1 || selled.lockNum > 70) || (selled.stockNum < 1 || selled.stockNum > 80)
+        || (selled.barrelNum < 1 || selled.barrelNum > 90)) {
+        return boost::none;
+    }
+
+    const auto total_profit =
+        (45 * selled.lockNum) + (30 * selled.stockNum) + (25 * selled.barrelNum);
+
+    if (total_profit >= 1800) {
+        return (0.10 * 1000.0) + (0.15 * 800.0) + (total_profit - 1800.0) * 0.2;
+
+    } else if (total_profit >= 1000) {
+        return (0.10 * 1000.0) + (total_profit - 1000.0) * 0.15;
+
+    } else {
+        return 0.10 * total_profit;
+    }
 }
